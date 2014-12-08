3b3f6fedb433f6bc435074e6ea2214ffeb43ff62
kubov <kubov2@gmail.com>
2014-12-08 20:30:40 +0100

    wip

--- modules/gateway/gateway.py
+++ modules/gateway/gateway.py
@@ -1,37 +1,31 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
-from twisted.python import log
 
-import time, sys
+import time, sys, json
 
 class Gateway(irc.IRCClient):
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
-        self.logger = MessageLogger(open(self.factory.filename, "a"))
-        self.logger.log("[connected at %s]" % 
-                        time.asctime(time.localtime(time.time())))
 
     def connectionLost(self, reason):
+        print "bay"
         irc.IRCClient.connectionLost(self, reason)
-        self.logger.log("[disconnected at %s]" % 
-                        time.asctime(time.localtime(time.time())))
-        self.logger.close()
-
 
     # callbacks for events
 
     def signedOn(self):
         """Called when bot has succesfully signed on to server."""
-        self.join(self.factory.channel)
+        for channel in self.factory.channels:
+            print "Joining ", channel
+            self.join(channel.encode('ascii'))
 
     def joined(self, channel):
         """This will get called when the bot joins the channel."""
-        self.logger.log("[I have joined %s]" % channel)
 
     def privmsg(self, user, channel, msg):
         """This will get called when the bot receives a message."""
         user = user.split('!', 1)[0]
-        self.logger.log("<%s> %s" % (user, msg))
+
         # Check to see if they're sending me a private message
         if channel == self.nickname:
             msg = "It isn't nice to whisper!  Play nice with the group."
@@ -42,12 +36,10 @@ class Gateway(irc.IRCClient):
         if msg.startswith(self.nickname + ":"):
             msg = "%s: I am a log bot" % user
             self.msg(channel, msg)
-            self.logger.log("<%s> %s" % (self.nickname, msg))
 
     def action(self, user, channel, msg):
         """This will get called when the bot sees someone do an action."""
         user = user.split('!', 1)[0]
-        self.logger.log("* %s %s" % (user, msg))
 
     # irc callbacks
 
@@ -55,7 +47,7 @@ class Gateway(irc.IRCClient):
         """Called when an IRC user changes their nickname."""
         old_nick = prefix.split('!')[0]
         new_nick = params[0]
-        self.logger.log("%s is now known as %s" % (old_nick, new_nick))
+
 
 
     # For fun, override the method that determines how a nickname is changed on
@@ -69,38 +61,38 @@ class Gateway(irc.IRCClient):
 
 
 
-class LogBotFactory(protocol.ClientFactory):
-    """A factory for LogBots.
-
-    A new protocol instance will be created each time we connect to the server.
-    """
-
-    def __init__(self, channel, filename):
-        self.channel = channel
-        self.filename = filename
+class GatewayFactory(protocol.ClientFactory):
+    def __init__(self, channels, nickname):
+        self.channels = channels
+        self.nickname = nickname
 
     def buildProtocol(self, addr):
-        p = LogBot()
+        p = Gateway()
         p.factory = self
         return p
 
     def clientConnectionLost(self, connector, reason):
-        """If we get disconnected, reconnect to server."""
         connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
         print "connection failed:", reason
         reactor.stop()
 
+class IrcBot:
+    def __init__(self, settings):
+        self.server   = settings["server"]
+        self.port     = settings["port"]
+        self.channels = settings["channels"]
+        self.nickname = settings["nickname"]
+
+    def run(self):
+        self.f = GatewayFactory(self.channels, self.nickname)
+        reactor.connectTCP(self.server, self.port, self.f)
+
+        print "Starting reactor"
+        reactor.run()
 
 if __name__ == '__main__':
-    # initialize logging
-    log.startLogging(sys.stdout)
-    # create factory protocol and application
-    f = LogBotFactory(sys.argv[1], sys.argv[2])
+    data = open("settings.json").read()
 
-    # connect factory to this host and port
-    reactor.connectTCP("irc.freenode.net", 6667, f)
-
-    # run bot
-    reactor.run()
+    IrcBot(json.loads(data)).run()
