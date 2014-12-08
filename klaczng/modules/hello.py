62ce82289822cb37d44507a0ed113effd069d739
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2014-12-08 23:54:16 +0100

    Restructure project, fix spawn bug

--- klaczng/modules/hello.py
+++ klaczng/modules/hello.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 
-import rmq
+from klaczng import rmq
 
 
 class Hello(rmq.Rmq):
@@ -10,8 +10,3 @@ class Hello(rmq.Rmq):
         body['content'] = 'Hello!'
         self.publish('klacz.gateway', body=json.dumps(body))
 
-if __name__ == '__main__':
-    t = Hello()
-    queue = str(uuid.uuid4())
-    t.queue_bind(queue, 'events', 'klacz.privmsg.hello')
-    t.start_recieving(queue)
